a802bbd21810075ad7391f1fb118ca2377edc2a4
AmirPrf <amirf9938@gmail.com>
2023-07-31 06:02:23 +0330

    Identity Entity and user role Class

---
+++ NadinProductTask.Domain/Identity/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NadinProductTask.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadinProductTask.Domain.Identity
+{
+	public class ApplicationUser : IdentityUser
+	{
+		public ApplicationUser(string username) : base(username)
+		{
+		}
+
+		private ApplicationUser()
+		{ }
+
+	}
+}
